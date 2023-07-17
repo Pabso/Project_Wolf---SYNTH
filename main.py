099ae79b1a5d77546dfb26b31e914d61a11493de
Pabso <yogpast45@gmail.com>
2023-07-17 15:47:15 +1000

    Python - UNTESTED implemented seral comunication
    Reorganisation

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import time as t
+import serial
 import --- as image_ai
 
 ERROR = False
+max_freq_in = 130
+interval = 10
+
+neg_list = []
+pos_list = []
+
+arduino = serial.Serial(port='COM1', baudrate=115200, timeout=.1)
 
 # --------------- Read Word file and load into array-------------------
 word_file = open("words.txt","r")
 
 word_bank = [] #Word bank array to query from 
 
-for x in freq_in/interval:
+for x in max_freq_in/interval:
     word_bank.append(word_file.readline())
 
 word_file.close()
@@ -20,10 +28,12 @@ while 1:
     
     # take inputs from max live --- EXAMPLE CODE -------------
 
-    freq_in = 250
-    sign = 0
-    interval = 10
+    def write_read():
+        data = arduino.readline()
+        return data
 
+    freq_in = write_read()
+    
 
     #-------------- Set arrays -------------------
 
@@ -41,3 +51,11 @@ while 1:
     # ------------ Send to AI -----------------
 
     image_ai(pos_list, neg_list)
+
+
+    #Refreshing image checker --- Set at 12 words maximum
+
+    if (len(neg_list) + len(pos_list)) >= 12:
+        neg_list = []
+        pos_list = []
+
