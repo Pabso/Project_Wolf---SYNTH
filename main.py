c463b5a38aa6bd13d4a30c265a95f810b03b567f
pabso <yogpast45@gmail.com>
2023-10-17 17:41:35 +1100

    Testing config writing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time as t
-import serial
 import subprocess
+import socket
 
 def rounder_x(number, interval):
     output = 0
@@ -10,68 +10,121 @@ def rounder_x(number, interval):
     else:
         output = number - (number%interval)
 
-    return output
+    return int(output)
 
+print("Initalising...")
 
+#initalise Variables
 ERROR = False
-max_freq_in = 10000
-interval = 500
+SENT = False
+max_freq_in = 10000 #Hz
+interval = 500 #Hz
+NUM_OF_PROMPTS = 3 #number of prompts in image
+PROMPT_FRAMES = 50
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
 
-neg_list = []
-pos_list = []
+#File Definitions
+Word_Prompts_PATH = "prompts.txt"
+Animation_Settings_Temp_PATH = "AnimationSettings_temp.txt"
+Animation_Settings_PATH = "DeforumStableDiffusionLocal/runSettings.txt"
 
-arduino = serial.Serial(port='COM1', baudrate=115200, timeout=.1)
+
+prompts = [] #Sentences to be loaded 
+Config_file = [] #Temporary loading of file to be altered 
+prompt_list = [] #Loaded Prompts
+
+
+# --------------- Establish UDP Connection ------------------
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
 
 # --------------- Read Word file and load into array-------------------
-word_file = open("words.txt","r")
+with open(Word_Prompts_PATH,"r") as openfileobject:
+    for line in openfileobject:
+        prompts.append(line)
 
-word_bank = [] #Word bank array to query from 
+openfileobject.close()
 
-for x in max_freq_in/interval:
-    word_bank.append(word_file.readline())
+#---------------- Read Config FIle for editing ------------------------
+with open(Animation_Settings_Temp_PATH,"r") as openfileobject:
+    for line in openfileobject:
+        Config_file.append(line)
 
-word_file.close()
+openfileobject.close()
 
+print("Initalisation Complete")
 
-while 1:
+## MAIN ##
+while ERROR == False:
+    #inits
+    print("")
+    data = -1
+    SENT = False
+
+    #Load new Data
+    while SENT == False:
+        data, addr = ["300","0x3425425352"]#sock.recvfrom(1024) # buffer size is 1024 bytes
+        #print("received message: %s" % data)
+        
+        #Check if New Data Avalible
+        if data != -1:
+            freq_in = int(data)
+            SENT = True
+            print("Data Received")
+        else:
+            print("No Data Received")
+    
+
     #Set small timer may be changed depends on how inputs from max live are managed 
-    t.sleep(4)
-    
-    # take inputs from max live --- EXAMPLE CODE -------------
-    def write_data():
-        data = arduino.readline()
-        data = data.decode()
-        data = data.rstrip("\r\n")
-        data = data.split(" ")
-        return data
-    
-    print(write_data())
+    t.sleep(1)
 
-    freq_in = write_data()[0]
-    sign = write_data()[1]
+    #Load new prompts
+    prompt_list.append( prompts[int(rounder_x(freq_in, interval)/interval)] )
 
-    #-------------- Set arrays -------------------
+    #print(prompt_list)
+
+    #Once ENought prompts
+    if (len(prompt_list) >= NUM_OF_PROMPTS):
+
+        #init
+        FrameCounter = 0 
+
+        #Open new file to write to
+        Settings_file = open(Animation_Settings_PATH,"w") 
+
+        #Load Data into written file till prompt inputs
+        for i in range(len(Config_file)):
+            print(type(Config_file[i]))
+            if Config_file[i] == str("prompts_ani"):
+                print("Configuring Prompts")
+
+                ani_prompt_line = '"animation_prompts":{'
+
+                for pr in range(len(prompt_list)):
+                    ani_prompt_line = ani_prompt_line + '"'+ str(FrameCounter) + '"' + ':' + '"' + prompt_list[pr] +'"'
+
+                    if(pr < prompt_list):
+                        ani_prompt_line = ani_prompt_line + ","
+
+                    FrameCounter =+ PROMPT_FRAMES
+
+                ani_prompt_line = ani_prompt_line + "},\n"
+
+                Settings_file.writelines(ani_prompt_line)
+            else:
+                Settings_file.writelines(Config_file[i])
+            
+        
+
+        #Close File
+        Settings_file.close()
+
+        #Call AI Generation
+        #subprocess.call("python", "DeforumStableDiffusionLocal/run.py", "--enable_animation_mode", "--settings", '"runSettings.txt"' )
+
+        #Clean Inputs
+        prompt_list = []
 
 
-    in_word = word_bank[(rounder_x(freq_in, interval)/interval)]
-
-    if sign == 0:
-        neg_list.append(in_word)
-    elif sign == 1:
-        pos_list.append(in_word)
-    else:
-        print("ERROR -- Failed to determine sign")
-        ERROR = True
-        break
-
-    # ------------ Send to AI -----------------
-    prompt = "a phtograph containing" + pos_list + "but not " + neg_list
-
-    subprocess.call("python", "scripts/txt2img.py", "--prompt ", prompt, "--plms " )
-
-
-    #Refreshing image checker --- Set at 12 words maximum
-
-    if (len(neg_list) + len(pos_list)) >= 12:
-        neg_list = []
-        pos_list = []
